5141d40fc65fa163f7a75c72512c8e42b1b5b586
lavanshedr <lavanshedr@gmail.com>
2020-10-24 18:09:30 +0600

    change unsafe code to safe

--- CellularAutomaton/Drawer.cs
+++ CellularAutomaton/Drawer.cs
@@ -10,41 +10,14 @@ namespace CellularAutomata
 {
     static class BitmapExtension
     {
-        //Copied from https://docs.microsoft.com/en-us/dotnet/api/system.windows.media.imaging.writeablebitmap?view=netcore-3.1
-        public static unsafe void SetPixelValue(this WriteableBitmap bitmap, int column, int row, Color color)
+        public static void SetPixelValue(this WriteableBitmap bitmap, int column, int row, Color color)
         {
-            try
-            {
-                // Reserve the back buffer for updates.
-                bitmap.Lock();
-
-                unsafe
-                {
-                    // Get a pointer to the back buffer.
-                    IntPtr pBackBuffer = bitmap.BackBuffer;
-
-                    // Find the address of the pixel to draw.
-                    pBackBuffer += row * bitmap.BackBufferStride;
-                    pBackBuffer += column * 4;
-
-                    // Compute the pixel's color.
-                    int colorValueRGB = color.R << 16; // R
-                    colorValueRGB |= color.G << 8;   // G
-                    colorValueRGB |= color.B << 0;   // B
-
-
-                    // Assign the color data to the pixel.
-                    *((int*)pBackBuffer) = colorValueRGB;
-                }
-
-                // Specify the area of the bitmap that changed.
-                bitmap.AddDirtyRect(new Int32Rect(column, row, 1, 1));
-            }
-            finally
-            {
-                // Release the back buffer and make it available for display.
-                bitmap.Unlock();
-            }
+            Int32Rect rect = new Int32Rect(
+                column,
+                row,
+                1,
+                1);
+            bitmap.WritePixels(rect, new byte[] {color.B, color.G, color.R, 0}, 4, 0);
         }
     }
 }
